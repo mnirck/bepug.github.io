77e1d63fed026fab1e23c2f7b2d71dd86e7fd2e7
Stijnc <stijn.callebaut@outlook.be>
2018-09-25 19:57:22 +0000

    20180925.1

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "June 7, 2018 meetup",
+        "excerpt":"  Moving Your DSC Pull Server into the future by Ben Gelens  Flexible PowerShell logging without reinventing the wheel by Kim Oppalfens  My PowerShell development aids, Stijn CallebautHosted at itnetX Belgium Register here!Github slides and demo ","categories": ["meetup"],
+        "tags": [],
+        "url": "https://bepug.github.io/website/meetup/2018/06/07/meetup/",
+        "teaser":null},{
+        "title": "September 27, 2018 meetup",
+        "excerpt":"Infrastructure-as-Code: VSTS, Azure and the glue of PowerShell by Jeff Wouters Azure Cloud Shell and SHiPS by Steven Taylor Stick to the full potential of PowerShell by Christof Van Geendertaelen Function ctrl-c {} by Merlijn Van Waeyenberghe Too late for a complex ARM template by Ivo HaagenHosted at XylosRegister here!Github...","categories": ["meetup"],
+        "tags": [],
+        "url": "https://bepug.github.io/website/meetup/2018/09/27/meetup/",
+        "teaser":null}]
